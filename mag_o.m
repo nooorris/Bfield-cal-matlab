8b83dda5da6ba27208e10ec6eafa0a6959a3fe6c
nooorris <35596938+nooorris@users.noreply.github.com>
2018-02-11 09:37:38 +0000

    Add files via upload
    read hdf5 and plot Bfield in x,y,z direction and calculate the magnitude

--- mag_o.m
+++ mag_o.m
@@ -1,7 +1,7 @@
-clc;clear;
+clc;clear;close all;
 %% running parameters
 DUMP_PERIOSITY=500;
-DUMP_NUM=65;
+DUMP_NUM=100;                                 % need adjustment 
 TIME_STEP=46.161969725E-15;
 
 %% grid parameters
@@ -12,25 +12,50 @@ Gridlen = (GridMax - GridMin)./Cells;
 
 
 %% observed point parameters
-point = [10e-3 28.866e-3 8.5e-3];
+point = [10e-3 28.866e-3 8.5e-3];             % need adjustment 
 indices= ceil((point-GridMin)./Gridlen);
 
-%% 
-READ_INT=2;
+%% initialize and read setting
+READ_INT=2;                                   % need adjustment 
 sample=0:READ_INT:DUMP_NUM;
+Bxfield=zeros(1,length(sample));
+Byfield=zeros(1,length(sample));
 Bzfield=zeros(1,length(sample));
 time = zeros(1,length(sample));
 
 for i = 1:length(time)
-    a=h5read(['vsim_B_' int2str(sample(i)) '.h5'],'/B');
+    a=h5read(['vsim_B_' int2str(sample(i)) '.h5'],'/B',[1,indices(3),indices(2),indices(1)],[3,2,2,2]);
     time(i)= h5readatt(['vsim_B_' int2str(sample(i)) '.h5'],'/B','time');
-    Bzfield(i)=a(3,indices(3),indices(2),indices(1));
+    Bzfield(i)=a(3,1,1,1);
+    Byfield(i)=a(2,1,1,1);
+    Bxfield(i)=a(1,1,1,1);
 end
+    Bfield=sqrt(Bxfield.^2+Byfield.^2+Bzfield.^2);
+%% plot setting
+figure
+subplot(2,2,1)
+plot(time,Bxfield,'*--');
+xlabel('time/s');
+ylabel('Bx field/T');
+title('10nc Bx field in the center of ring')
 
+subplot(2,2,2)
+plot(time,Byfield,'*--');
+xlabel('time/s');
+ylabel('By field/T');
+title('10nc By field in the center of ring')
 
+subplot(2,2,3)
 plot(time,Bzfield,'*--');
 xlabel('time/s');
-ylabel('magnetic field/T');
-title('100nc Bz field in the center of ring')
+ylabel('Bz field/T');
+title('10nc Bz field in the center of ring')
+
+
+subplot(2,2,4)
+plot(time,Bfield,'*--');
+xlabel('time/s');
+ylabel('B field/T');
+title('10nc B field in the center of ring')
 
 
